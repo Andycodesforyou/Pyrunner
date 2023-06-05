f31b25a994f43bf4dcab37b209eddde7574e9fb3
Andy John <Johnny@AndyJohn.local>
2023-06-04 20:30:50 -0600

    More progess on game

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # at 18:45 framerate can change notes
-# at 1:08:15 follow along (Maybe start a few minutes back)
+# at 1:21:40 follow along documentation reference draw line - (already done but good practice)
 
 """
 git init
@@ -30,7 +30,8 @@ sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
 #Text surface
-text_surface = test_font.render('My game', False, 'Black').convert()
+score_surface = test_font.render('My game', False, 'Black').convert()
+score_rect = score_surface.get_rect(center = (400, 50))
 
 #Snail Surface
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
@@ -47,10 +48,21 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    #Mouse stuff
+
+        # if event.type == pygame.MOUSEMOTION:
+        #   if player_rect.collidepoint(event.pos): print('collision')
+
     #Background
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
-    screen.blit(text_surface, (300,50))
+    
+    #text/score
+    pygame.draw.rect(screen, 'White', score_rect, 20)
+    pygame.draw.rect(screen, 'White', score_rect)
+   
+    screen.blit(score_surface, score_rect)
 
     #snail animation 
     snail_rect.x -=4
@@ -66,9 +78,12 @@ while True:
 
 
     #Collision detection
-    if player_rect.colliderect(snail_rect):
-        print('collision')
-
-
+    """if player_rect.colliderect(snail_rect):
+        print('collision')"""
+    """
+    mouse_pos = pygame.mouse.get_pos()
+    if player_rect.collidepoint((mouse_pos)):
+        print(pygame.mouse.get_pressed())
+    """
     pygame.display.update()
     clock.tick(60)
